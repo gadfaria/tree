3340eb02a69818a36515f9b78f69ff8b68dd7fd7
Gabriel Faria <gadfaria@gmail.com>
2023-08-31 22:59:39 -0300

    remove cl

--- src/App.tsx
+++ src/App.tsx
@@ -341,7 +341,6 @@ function timeElapse(date: Date) {
     seconds +
     "</span> segundos";
 
-  // $("#clock").html(result);
   const clock = document.getElementById("clock") as HTMLDivElement;
   clock.innerHTML = result;
 }
@@ -351,16 +350,16 @@ function random(min: number, max: number) {
 }
 
 function bezier(cp: IPoint[], t: number) {
-  var p1 = cp[0].mul((1 - t) * (1 - t));
-  var p2 = cp[1].mul(2 * t * (1 - t));
-  var p3 = cp[2].mul(t * t);
+  let p1 = cp[0].mul((1 - t) * (1 - t));
+  let p2 = cp[1].mul(2 * t * (1 - t));
+  let p3 = cp[2].mul(t * t);
   return p1.add(p2).add(p3);
 }
 
 function inheart(x: number, y: number, r: number) {
   // x^2+(y-(x^2)^(1/3))^2 = 1
   // http://www.wolframalpha.com/input/?i=x%5E2%2B%28y-%28x%5E2%29%5E%281%2F3%29%29%5E2+%3D+1
-  var z =
+  let z =
     ((x / r) * (x / r) + (y / r) * (y / r) - 1) *
       ((x / r) * (x / r) + (y / r) * (y / r) - 1) *
       ((x / r) * (x / r) + (y / r) * (y / r) - 1) -
@@ -524,7 +523,6 @@ class Tree implements ITree {
 
   initBranch() {
     let branchs = this.opt.branch || [];
-    console.log(branchs);
     this.branchs = [];
     this.addBranchs(branchs);
   }
@@ -552,14 +550,14 @@ class Tree implements ITree {
   }
 
   draw(k: string) {
-    var s = this,
-      ctx = s.ctx;
-    var rec = s.record[k];
+    let s = this;
+    let ctx = s.ctx;
+    let rec = s.record[k];
     if (!rec) {
       return;
     }
-    var point = rec.point,
-      image = rec.image;
+    let point = rec.point;
+    let image = rec.image;
 
     ctx.save();
     ctx.putImageData(image, point.x, point.y);
